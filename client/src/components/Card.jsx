c257afe7466d11078b65bd49b6f9bae047dfe06c
Monica Tang <monicatangd@hotmail.com>
2023-04-07 00:06:39 -0500

    front

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,7 +4,7 @@ import "./Card/Card.css";
 export default function Card({image, name, temperament, weight}){
     return(
         
-        <div className="car">
+        <div className="card">
             <img src={image}  alt="img not found"/>
             <h3>{name}</h3>
             <h5>Temperament: {temperament}</h5>
